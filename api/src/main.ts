73e67584180d96cd761b9f7d18a549ca3b812715
Willem Franco <willemfranco@gmail.com>
2023-01-15 01:36:09 -0500

    enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,8 @@ import { initSwagger } from './app.swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   initSwagger('docs', app);
 
   app.useGlobalPipes(
