066331634f74e9f539cb412afd707e174df9acb2
Willem Franco <willemfranco@gmail.com>
2023-01-16 14:00:56 -0500

    allow search suggestions

--- app/src/app/pages/meals/meals.service.ts
+++ app/src/app/pages/meals/meals.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CrudService } from 'src/app/shared/HttpService';
+import { CrudService, Options } from 'src/app/shared/HttpService';
 import { Meal } from './meals.interfaces';
 
 @Injectable({
@@ -7,4 +7,27 @@ import { Meal } from './meals.interfaces';
 })
 export class MealsService extends CrudService<Meal> {
   override uri = 'meals';
+
+  find(query: string, options?: Options) {
+    return this.http.get<Meal[]>(`find/${query}`, options);
+  }
+}
+
+export function mergeTags(input: Meal, value: string) {
+  if (!/\s$/.test(value)) {
+    return value.trim();
+  } else {
+    const tags = value.trim();
+    if (tags) {
+      let merged = input.tags;
+      tags.split(/\s+/).forEach((tag) => {
+        if (!merged.find((t) => t === tag)) {
+          if (merged === input.tags) merged = input.tags.slice();
+          merged.push(tag);
+        }
+      });
+      input.tags = merged;
+    }
+    return '';
+  }
 }
